436e9e81eae7daf16c5ac4f0325c4378b14d0317
JeanPaul Willard <jeanpaul.willard@gmail.com>
2026-06-15 06:32:32 -0300

    Refactor: split monolithic index.html into css/, js/, assets/ structure

--- sw.js
+++ sw.js
@@ -1,5 +1,33 @@
-const CACHE = 'calitrack-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'calitrack-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './assets/ding.mp3',
+  './css/base.css',
+  './css/components.css',
+  './css/workout.css',
+  './css/meals.css',
+  './css/schedule.css',
+  './css/notes.css',
+  './css/today.css',
+  './css/data.css',
+  './css/body.css',
+  './css/challenges.css',
+  './js/state.js',
+  './js/utils.js',
+  './js/workout.js',
+  './js/meals.js',
+  './js/schedule.js',
+  './js/notes.js',
+  './js/data.js',
+  './js/today.js',
+  './js/body.js',
+  './js/challenges.js',
+  './js/init.js',
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
